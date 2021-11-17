2b3797713ad058a42320a5afacfb316abd9b9fef
Jacob Yablonski <jyablonski9@gmail.com>
2021-11-17 19:33:18 +0000

    adding opp color variables

--- models/marts/prod_recent_games_teams.sql
+++ models/marts/prod_recent_games_teams.sql
@@ -11,11 +11,13 @@ with recent_games as (
         team_avg_score,
         pts_color,
         team_logo,
-        opp_logo
+        opp_logo,
+        'Vs.' as new_loc
     from {{ ref('prep_recent_games_teams')}}
 
 ),
 
+/* only grabbing latest games, the prep table has everything for qa purposes */
 recent_date as (
     select
         max(date) as date
@@ -31,3 +33,4 @@ team_pts_scored as (
 
 select *
 from team_pts_scored
+where outcome = 'W'
