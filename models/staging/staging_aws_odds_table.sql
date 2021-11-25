ab6c1f1d4177cfbd3cc472d0135e4c5a0d84f098
Jacob Yablonski <jyablonski9@gmail.com>
2021-11-25 19:36:48 +0000

    minor fixes

--- models/staging/staging_aws_odds_table.sql
+++ models/staging/staging_aws_odds_table.sql
@@ -2,7 +2,7 @@ SELECT  team::text as team,
         {{convert_team_names('team')}} as team_acronym,
         spread::numeric as spread,
         total::numeric as total,
-        moneyline::numeric as moneyline, 
+        coalesce(moneyline, 100)::numeric as moneyline, 
         datetime1::timestamp as time, 
         date::date as date
 FROM {{ source('nba_source', 'aws_odds_source')}}
