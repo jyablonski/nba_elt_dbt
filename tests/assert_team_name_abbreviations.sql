3db3ef4078d35f64c4cf37e2e9b49c5401fdf436
Jacob Yablonski <jyablonski9@gmail.com>
2021-12-27 20:00:50 +0000

    adding moneyline odds test

--- tests/assert_team_name_abbreviations.sql
+++ tests/assert_team_name_abbreviations.sql
@@ -4,4 +4,5 @@ WHERE team NOT IN ('ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DAL', 'DEN', 'DET'
                    'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA', 'MIL', 'MIN', 'NOP', 'NYK',
                    'OKC', 'ORL', 'PHI', 'PHX', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS')
 
-/* the idea being that we want this to return 0 rows, if something gets returned that means its passing further downstream which is a problem */
+/* the idea being that we want this to return 0 rows, if something gets returned that means its passing further downstream which is a problem 
+   12-27-21 update: this is actually not needed bc it can just be done via a column value test w/ dbt expectations */
