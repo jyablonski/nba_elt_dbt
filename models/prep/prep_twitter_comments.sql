a41580d977c99e3d01d6ac5dae62e8022f372535
Jacob Yablonski <jyablonski9@gmail.com>
2022-01-22 18:17:44 +0000

    fixing duplicate tweets

--- models/prep/prep_twitter_comments.sql
+++ models/prep/prep_twitter_comments.sql
@@ -1,41 +1,35 @@
 with twitter_cte as (
     select
-        *
+        distinct *
     from {{ ref('staging_aws_twitter_data_table') }}
 ),
 
-final as (
+-- HANDLING DUPLICATES
+-- grabbing the tweet with highest likes_count (assuming this is the most up to date - not most correct way of doing this but w.e)
+duplicate_tweets as (
     select 
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY username, tweet, scrape_date
+            ORDER BY likes_count desc
+        ) as tweet_rank
+    from twitter_cte
+),
+
+final as (
+    select
         *,
         row_number() over (order by likes_count desc) as likes_rank_ytd,
         row_number() over (order by retweets_count desc) as retweets_rank_ytd,
         row_number() over (order by replies_count desc) as replies_rank_ytd,
         case when likes_count > 0 then round(replies_count / likes_count, 3)::numeric
         else 0 end as controversial_score
-    from twitter_cte
-),
-
--- HANDLING DUPLICATES
--- grabbing the tweet with highest likes_count (assuming this is the most up to date - not most correct way of doing this but w.e)
-duplicate_tweets as (
-    select
-        username,
-        tweet,
-        scrape_date,
-        max(likes_count) as likes_count,
-        max(created_at) as created_at
-    from twitter_cte
-    group by 1, 2, 3
-),
-
-final2 as (
-    select
-        *
-    from final
-    inner join duplicate_tweets using (username, tweet, scrape_date, likes_count, created_at)
+    from duplicate_tweets
+    where tweet_rank = 1
 )
 -- where username = 'poa_prayer'
+-- where username = 'mehhhaccount'
 
 select *
-from final2
+from final
 order by likes_count desc
