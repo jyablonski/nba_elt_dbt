79dd2bb8c0c6202d12ae9eaf591244cf143edcf0
Jacob Yablonski <jyablonski9@gmail.com>
2022-01-22 21:46:14 +0000

    adding social media analysis bans

---
+++ models/prep/prep_twitter_aggs.sql
@@ -0,0 +1,43 @@
+with my_cte as (
+    select
+        *
+    from {{ ref('staging_aws_twitter_data_table') }}
+),
+
+aggs as (
+    select 
+        scrape_date as date,
+        count(*) as twitter_tot_comments,
+        round(avg(compound), 3) as avg_compound,
+        round(avg(pos), 3) as avg_pos,
+        round(avg(neg), 3) as avg_neg,
+        round(avg(neu), 3) as avg_neu,
+        'join' as join_col
+    from my_cte
+    group by 1
+),
+
+tot_aggs as (
+    select
+        avg(twitter_tot_comments) as twitter_avg_comments,
+        'join' as join_col
+    from aggs
+),
+
+final as (
+    select
+        date,
+        twitter_tot_comments,
+        avg_compound,
+        avg_pos,
+        avg_neg,
+        avg_neu,
+        twitter_avg_comments,
+        round(twitter_tot_comments - twitter_avg_comments, 1)::numeric as count_differential,
+        round((twitter_tot_comments - twitter_avg_comments) / twitter_avg_comments, 3)::numeric * 100 as twitter_pct_difference
+    from aggs
+    left join tot_aggs using (join_col)
+)
+
+select *
+from final
