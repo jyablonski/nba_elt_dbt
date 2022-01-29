ed2faf8520a22cb32bcbb3d2e3a0e582c96a2000
Jacob Yablonski <jyablonski9@gmail.com>
2022-01-29 15:59:33 +0000

    testing slim ci #minor

--- models/marts/prod_team_contracts_analysis.sql
+++ models/marts/prod_team_contracts_analysis.sql
@@ -1,6 +1,11 @@
 with my_cte as (
     select
-        *
+        team,
+        win_percentage,
+        sum_salary_earned,
+        sum_salary_earned_max,
+        team_pct_salary_earned,
+        record
     from {{ ref('prep_team_contracts_analysis')}}
 )
 
