96e958dbd4a5bc3c17e636b837ad59b839a27c63
Jacob Yablonski <jyablonski9@gmail.com>
2022-02-13 16:05:29 +0000

    adding inactive dates for allstar break

---
+++ models/prep/prep_social_media_team_aggs.sql
@@ -0,0 +1,29 @@
+with my_cte as (
+    select
+        author,
+        score,
+        coalesce(flair_final, 'No Flair')::text as flair,
+        compound,
+        neg,
+        neu,
+        pos
+    from {{ ref('prep_reddit_comments') }}
+),
+
+aggs as (
+    select
+        flair,
+        count(*) as num_count,
+        round(avg(score), 3) as avg_score,
+        round(avg(compound), 3) as avg_compound,
+        round(avg(neg), 3) as avg_neg,
+        round(avg(neu), 3) as avg_neu,
+        round(avg(pos), 3) as avg_pos
+    from my_cte
+    group by 1
+    order by avg_compound desc
+)
+
+select *
+from aggs
+where num_count > 50
