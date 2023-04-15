6071b2df4273015772f74d65119683f4e81dcb44
jyablonski9 <jyablonski9@gmail.com>
2023-04-15 08:41:20 -0500

    update seasont ype macro

--- macros/generate_season_type.sql
+++ macros/generate_season_type.sql
@@ -1,6 +1,6 @@
 {% macro generate_season_type(column_name) %}  
     case when {{ column_name }} < '2023-04-11' then 'Regular Season'
-         when {{ column_name }} >= '2023-04-11' and {{ column_name }} < '2023-04-16' then 'Play-In'
+         when {{ column_name }} >= '2023-04-11' and {{ column_name }} < '2023-04-15' then 'Play-In'
          else 'Playoffs' 
     END
 {%- endmacro %}
