5ae0b6637ee83b0b46cd7d9f9fd57b19ff94aaba
jyablonski9 <jyablonski9@gmail.com>
2023-05-27 18:03:00 -0500

    add views

---
+++ models/marts/user_past_predictions.sql
@@ -0,0 +1,36 @@
+{{ config(materialized='view') }}
+
+with home_wins as (
+    select
+        full_team as home_team,
+        date as game_date,
+        outcome
+    from {{ ref('mov') }}
+),
+
+combo as (
+    select 
+        id,
+        username,
+        user_predictions.game_date,
+        user_predictions.home_team,
+        home_team_odds,
+        home_team_predicted_win_pct,
+        away_team,
+        away_team_odds,
+        away_team_predicted_win_pct,
+        selected_winner,
+        created_at,
+        case when outcome = 'W' then home_team
+            when outcome = 'L' then away_team
+            else 'TBD' end as actual_winner
+    from {{ source('nba_prod', 'user_predictions') }}
+    left join home_wins using (home_team, game_date)
+
+)
+
+select 
+    *,
+    case when selected_winner = actual_winner then 1
+        else 0 end as is_correct_prediction
+from combo
