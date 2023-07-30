e177379c8e2762912e77e489956b0b007d990ee5
jacob <jyablonski9@gmail.com>
2023-07-30 10:13:30 -0500

    dbt Prod Release v2.1.6 (#176)
    ### Description
    Adding SCD2 Table to track player team history in the event they were traded in the season
    
    ## Added
    - SCD2 Table for player team history.
      - Table is only in `nba_prep/`, will add it into an actual table in the front end at some point in the future
    
    ## Updated
    - None
    
    ## Deleted
    - Removed link to Netlify dbt Docs

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 ![Deployment](https://github.com/jyablonski/nba_elt_dbt/actions/workflows/deploy.yml/badge.svg)
 
-Version: 2.1.5
+Version: 2.1.6
 
 ### dbt Resources for [NBA Project](https://github.com/jyablonski/NBA-Dashboard)
-
-### dbt docs available [here](https://jyablonski.netlify.app/)
 
 1. Links to other Repos providing infrastructure for this Project
     * [Shiny Server](https://github.com/jyablonski/NBA-Dashboard)
@@ -12,5 +10,4 @@ Version: 2.1.5
     * [Terraform](https://github.com/jyablonski/aws_terraform)
     * [Airflow Proof of Concept](https://github.com/jyablonski/nba_elt_airflow)
     * [ML Pipeline](https://github.com/jyablonski/nba_elt_mlflow)
-    * [GraphQL API](https://github.com/jyablonski/graphql_praq)
     * [REST API](https://github.com/jyablonski/nba_elt_rest_api)
---
+++ models/prep/prep_players_teams_scd2.sql
@@ -0,0 +1,59 @@
+-- goal is to recreate `prep/prep_player_most_recent_team.sql`
+-- easier way to do with snowflake syntax and partition by / group bys in same cte
+-- https://github.com/dbt-labs/dbt-core/issues/3878
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'scd_id',
+) }}
+
+with player_records as (
+    select
+        player,
+        team,
+        date,
+        {{ dbt_utils.surrogate_key(["player", "team"]) }} as scd_id
+    from {{ ref('staging_aws_boxscores_incremental_table') }}
+	{% if is_incremental() %}
+
+	-- this filter will only be applied on an incremental run
+	-- only grab records where date is greater than the max date of the existing records in the tablegm
+	where date > (select max(valid_to) from {{ this }})
+
+	{% endif %}
+),
+
+max_dates as (
+    select
+        player,
+        max(date) as max_date
+    from player_records
+    group by player
+),
+
+player_team_effective_dates as (
+    select
+        scd_id,
+        player,
+        team,
+        count(*) as games_played_for_team,
+        min(date) as valid_from,
+        max(date) as max_game_date
+    from player_records
+    group by scd_id, player, team
+),
+
+final as (
+    select
+        scd_id,
+        player_team_effective_dates.player,
+        player_team_effective_dates.team,
+        games_played_for_team,
+        valid_from,
+        case when max_game_date = max_date then '9999-12-31' else max_game_date end as valid_to,
+        case when max_game_date = max_date then 1 else 0 end as is_current_team
+    from player_team_effective_dates
+    inner join max_dates using (player)
+)
+
+select *
+from final
