99cd9178b8885c3382999c6caa3fd15692abefa6
jyablonski9 <jyablonski9@gmail.com>
2023-10-28 16:33:28 -0500

    add 2 new cols for contracts analysis

--- models/marts/team_contracts_analysis.sql
+++ models/marts/team_contracts_analysis.sql
@@ -5,7 +5,9 @@ with my_cte as (
         sum_salary_earned,
         sum_salary_earned_max,
         team_pct_salary_earned,
-        record
+        record,
+        value_lost_from_injury,
+        team_pct_salary_lost
     from {{ ref('prep_team_contracts_analysis') }}
 )
 
