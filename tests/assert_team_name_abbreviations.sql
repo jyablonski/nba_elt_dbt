72cdf497264ed7d14d6d14dab8a656698e8d841e
jyablonski9 <jyablonski9@gmail.com>
2024-01-18 19:22:41 -0600

    ADD SOME GOODS

--- tests/assert_team_name_abbreviations.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM {{ ref('staging_aws_boxscores_incremental_table') }}
-WHERE team NOT IN ('ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW',
-                   'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA', 'MIL', 'MIN', 'NOP', 'NYK',
-                   'OKC', 'ORL', 'PHI', 'PHX', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS')
-
-/* the idea being that we want this to return 0 rows, if something gets returned that means its passing further downstream which is a problem 
-   12-27-21 update: this is actually not needed bc it can just be done via a column value test w/ dbt expectations */
