c64e42f2a2640cbb01fb37aaa38556025cdc12ce
jacob <jyablonski9@gmail.com>
2024-03-02 12:17:46 -0600

    dbt Prod Release v2.5.6 (#191)
    ### Description
    Adding Reporting Schema Views
    
    ## Added
    - Reporting Schema
    - `.gitattributes` File
    
    ## Updated
    - Moved `user_prediction_profit_by_team` View to Reporting Schema
    - Team Advanced Stats Model to add Rank Columns
    - Various Models to remove table aliasing bad practices
    
    ## Deleted
    - `sqlfluff` File, replaced with config options in `pyproject.toml`

--- models/prep/prep_boxscores_mvp_calc.sql
+++ models/prep/prep_boxscores_mvp_calc.sql
@@ -1,4 +1,4 @@
-with my_cte as (
+with boxscores as (
     select *
     from {{ ref('staging_aws_boxscores_incremental_table') }}
 ),
@@ -25,44 +25,47 @@ game_ids as (
 
 final as (
     select
-        g.player,
-        g.team,
-        g.date as game_date,
-        i.game_id,
-        g.location,
-        g.opponent,
-        g.outcome,
-        g.mp,
-        g.fgm,
-        g.fga,
-        g.fgpercent,
-        g.threepfgmade,
-        g.threepattempted,
-        g.threepointpercent,
-        g.ft,
-        g.fta,
-        g.ftpercent,
-        g.oreb,
-        g.dreb,
-        g.trb,
-        g.ast,
-        g.stl,
-        g.blk,
-        g.tov,
-        g.pf,
-        g.pts,
-        coalesce(g.plusminus, 0) as plus_minus,
-        g.gmsc,
-        g.season_type,
-        g.season,
-        {{ generate_ts_percent('g.pts', 'g.fga', 'g.fta::numeric') }} as game_ts_percent,
+        boxscores.player,
+        boxscores.team,
+        boxscores.date as game_date,
+        game_ids.game_id,
+        boxscores.location,
+        boxscores.opponent,
+        boxscores.outcome,
+        boxscores.mp,
+        boxscores.fgm,
+        boxscores.fga,
+        boxscores.fgpercent,
+        boxscores.threepfgmade,
+        boxscores.threepattempted,
+        boxscores.threepointpercent,
+        boxscores.ft,
+        boxscores.fta,
+        boxscores.ftpercent,
+        boxscores.oreb,
+        boxscores.dreb,
+        boxscores.trb,
+        boxscores.ast,
+        boxscores.stl,
+        boxscores.blk,
+        boxscores.tov,
+        boxscores.pf,
+        boxscores.pts,
+        coalesce(boxscores.plusminus, 0) as plus_minus,
+        boxscores.gmsc,
+        boxscores.season_type,
+        boxscores.season,
+        {{ generate_ts_percent('boxscores.pts', 'boxscores.fga', 'boxscores.fta::numeric') }} as game_ts_percent,
         round((
             pts::numeric + (0.5 * plusminus::numeric) + (2 * (stl::numeric + blk::numeric))
             + (0.5 * trb::numeric) - (1.5 * tov::numeric) + (1.5 * ast::numeric)
         ), 1)::numeric as game_mvp_score
-    from my_cte as g
-        left join game_ids as i using (team, date, opponent)
-
+    from boxscores
+        left join game_ids
+            on
+                boxscores.team = game_ids.team
+                and boxscores.date = game_ids.date
+                and boxscores.opponent = game_ids.opponent
 )
 
 select *
