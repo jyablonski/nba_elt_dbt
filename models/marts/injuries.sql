9e7316f8169c3a39166884219673c905c6de426c
jyablonski9 <jyablonski9@gmail.com>
2024-04-14 09:58:07 -0500

    mfer

--- models/marts/injuries.sql
+++ models/marts/injuries.sql
@@ -4,6 +4,7 @@ select
     teams.team_acronym,
     injury_data.injury,
     injury_data.injury_status,
-    injury_data.injury_description
+    injury_data.injury_description,
+    injury_data.scrape_date as scrape_date
 from {{ ref('injury_data') }}
     inner join {{ ref('teams') }} on injury_data.team = teams.team
