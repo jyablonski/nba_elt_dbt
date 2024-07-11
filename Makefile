b0f37572b2121e27152dc3b190471abd2b9decde
jacob <jyablonski9@gmail.com>
2024-07-11 17:52:02 -0500

    dbt v3.1.0 (#202)
    ### Description
    Updated dbt Version, added some new functionality, and included a new section to build dbt Docs & ship them to S3 in the Deployment Pipeline
    
    ## Added
    - Slack Alert following dbt Deploys
    - dbt Docs in S3 Pipeline
    - dbt Unit Test Example
    - dbt Fixture Example for Unit Tests
    
    ## Updated
    - `tests` -> `data_tests` in every `.yml` file to accommodate dbt v1.8 changes
    - Formatted `docker/Dockerfile`
    - dbt Version to 1.8.3
    
    ## Deleted
    - None

--- Makefile
+++ Makefile
@@ -48,3 +48,14 @@ test:
 
 run_dbt:
 	@docker compose -f docker/docker-compose-test.yml run dbt_runner dbt build --profiles-dir profiles/ --profile dbt_ci
+
+.PHONY: run-unit-tests
+run-unit-tests:
+	@docker compose -f docker/docker-compose-test.yml run dbt_runner dbt test --select test_type:unit --profiles-dir profiles/ --profile dbt_ci
+	@make down
+
+
+.PHONY: cd-docs-generate
+cd-docs-generate:
+	@docker compose -f docker/docker-compose-test.yml run dbt_runner dbt docs generate --profiles-dir profiles/ --profile dbt_ci
+	@make down
---
+++ tests/fixtures/teams_source_fixture.sql
@@ -0,0 +1,38 @@
+select
+    'Milwaukee Bucks' as team,
+    'MIL' as team_acronym,
+    'Eastern' as conference,
+    '#00471b' as primary_color,
+    '#f0ebd2' as secondary_color,
+    'NA' as third_color,
+    46 as previous_season_wins,
+    7 as previous_season_rank,
+    'https://stats.nba.com/media/img/teams/logos/MIL_logo.svg' as team_logo,
+    '2024-04-16 14:34:40.092' as created_at,
+    '2024-04-16 14:34:40.092' as modified_at
+union all
+select
+    'Toronto Raptors',
+    'TOR',
+    'Eastern',
+    '#ce1141',
+    '#061922',
+    'NA',
+    27,
+    24,
+    'https://stats.nba.com/media/img/teams/logos/TOR_logo.svg',
+    '2024-04-16 14:34:40.092',
+    '2024-04-16 14:34:40.092'
+union all
+select
+    'Boston Celtics',
+    'BOS',
+    'Eastern',
+    '#008348',
+    '#061922',
+    '#bb9753',
+    36,
+    16,
+    'https://stats.nba.com/media/img/teams/logos/BOS_logo.svg',
+    '2024-04-16 14:34:40.092',
+    '2024-04-16 14:34:40.092'
