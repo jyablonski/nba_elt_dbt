e6e3c2723a5b85d99c2aa526b6b44208480b0bd9
jacob <jyablonski9@gmail.com>
2024-11-16 11:37:01 -0800

    dbt v3.1.5 (#208)
    ### Description
    Updated `dbt-core` and added new QoL Test
    
    ## Added
    - New `test_missing_boxscores` Test to identify any dates in the past
    where boxscores weren't pulled
    
    ## Updated
    - `dbt-core` from `1.8.3` -> `1.8.8`
    
    ## Deleted
    - None

---
+++ tests/test_missing_boxscores.sql
@@ -0,0 +1,25 @@
+/* this test looks at all scraped boxscores and compares them to the full schedule table
+if there are any games played before today's date that have no records in the boxscores table,
+then this test will return an error */
+
+with boxscores_data as (
+    select distinct
+        {{ dbt_utils.generate_surrogate_key(['home_team_df.team', 'away_team_df.team', 'boxscores.game_date']) }} as game_pk,
+        home_team_df.team as home_team,
+        away_team_df.team as away_team,
+        boxscores.game_date
+    from {{ ref('boxscores') }}
+        inner join {{ ref('teams') }} as home_team_df
+            on boxscores.team = home_team_df.team_acronym
+        inner join {{ ref('teams') }} as away_team_df
+            on boxscores.opponent = away_team_df.team_acronym
+    where
+        location = 'H'
+        -- this is needed so we don't double up on records & games played
+)
+
+select *
+from {{ ref('prep_schedule_table') }}
+where
+    game_date < current_date
+    and game_pk not in (select game_pk from boxscores_data)
